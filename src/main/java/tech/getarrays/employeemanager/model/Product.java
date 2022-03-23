276340f07c2d70d687d9c6cb76639d6936d463b3
lencer <kutloanodelencer@gmail.com>
2022-03-23 11:58:25 +0200

    product model generation

--- src/main/java/tech/getarrays/employeemanager/model/Product.java
+++ src/main/java/tech/getarrays/employeemanager/model/Product.java
@@ -1,4 +1,10 @@
 package tech.getarrays.employeemanager.model;
 
 public class Product {
+    private Long id;
+    private String product_name;
+    private String price;
+    private String stock_level;
+    private String category;
+    private String imageUrl;
 }
